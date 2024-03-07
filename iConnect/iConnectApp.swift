f01e0e3b5b2ef86d5d17b6558580fd48bfe0387c
Min Pyae <minmp2002@gmail.com>
2024-03-07 22:10:28 +0000

    Initial Commit

---
+++ iConnect/iConnectApp.swift
@@ -0,0 +1,17 @@
+//
+//  iConnectApp.swift
+//  iConnect
+//
+//  Created by Chris Min on 07/03/2024.
+//
+
+import SwiftUI
+
+@main
+struct iConnectApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
